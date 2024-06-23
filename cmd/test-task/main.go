82c52fc9023d268f2d7daa9d64823aa9c98dc7a5
Влад <killyourselff1703@mail.ru>
2024-06-24 03:25:37 +0400

    service created

---
+++ cmd/test-task/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"awesomeProject/internal/pkg/app"
+	"log"
+)
+
+func main() {
+	a, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = a.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
